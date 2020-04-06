aaf04ed7dec8ef27728cde304a952219c7012c6f
Christian Albrecht <christian.albrecht@mayflower.de>
2020-04-06 19:59:02 +0200

    Glue it all together in default.nix

--- nixpkgs/default.nix
+++ nixpkgs/default.nix
@@ -1,12 +1,39 @@
-self: super:
+self: pkgs:
+
 let
-  import-overlay = path: import path self super;
-in with super; {
-  emacs27-git = import-overlay ./pkgs-overlays/emacs27-git.nix;
+  import-overlay = path: import path self pkgs;
+in with pkgs;
+{
   global-cursor-theme = callPackage ./pkgs/global-cursor-theme.nix {};
+
   paperclip-cli = callPackage ./pkgs/paperclip.nix {};
+
+  emacs27-git = import-overlay ./pkgs-overlays/emacs27-git.nix;
+
   pass = import-overlay ./pkgs-overlays/pass.nix;
 
-  vagrant = callPackage /home/alab/ws/vagrant {};
+  xdg-desktop-portal-gtk = import-overlay ./pkgs-overlays/xdg-desktop-portal-gtk.nix;
 
-} // (import-overlay ./pkgs-overlays/jetbrains.nix)
+} // {
+  # screensharing
+
+  firefox-wayland-pipewire-unwrapped = import-overlay ./pkgs-overlays/firefox.nix;
+
+  firefox-wayland-pipewire = wrapFirefox self.firefox-wayland-pipewire-unwrapped {
+    gdkWayland = true;
+  };
+
+  pipewire_0_3 = import-overlay ./pkgs-overlays/pipewire.nix;
+
+  xdg-desktop-portal = import-overlay ./pkgs-overlays/xdg-desktop-portal.nix;
+
+  xdg-desktop-portal-wlr = pkgs.callPackage ./pkgs/xdg-desktop-portal-wlr.nix {
+      pipewire = self.pipewire_0_3;
+  };
+
+} // {
+  waylandPkgs = (pkgs.waylandPkgs or {}) // {
+    inherit (self) mako xdg-desktop-portal-wlr waybar;
+  };
+} //
+(import-overlay ./pkgs-overlays/jetbrains.nix)
