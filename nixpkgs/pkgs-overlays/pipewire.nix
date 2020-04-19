e608ae40d4e8c0a0c82b2cbe263fa6b05a95042d
Christian Albrecht <christian.albrecht@mayflower.de>
2020-04-19 16:13:50 +0200

    Add latest pipewire

--- nixpkgs/pkgs-overlays/pipewire.nix
+++ nixpkgs/pkgs-overlays/pipewire.nix
@@ -3,18 +3,15 @@ self: pkgs:
 pkgs.pipewire.overrideAttrs (old: rec {
   version = "0.3.2";
 
-  src = pkgs.fetchFromGitHub {
-    owner = "PipeWire";
+  src = pkgs.fetchFromGitLab {
+    domain = "gitlab.freedesktop.org";
+    owner = "pipewire";
     repo = "pipewire";
-    rev = version;
-    sha256 = "1aqhaaranv1jlc5py87mzfansxhzzpawqrfs8i08qc5ggnz6mfak";
+    rev = "dfd1adf816dfee75ebec94f2de386fc73ececb02";
+    sha256 = "1garm8bv0jjknwyivr63xsmw8s3jfgf6j6ky1w5fn0010zjqviif";
   };
 
   buildInputs = old.buildInputs ++ (with self; [
-    libsndfile
-    bluez5
-    vulkan-loader
-    libpulseaudio
   ]);
 
   mesonFlags = [
