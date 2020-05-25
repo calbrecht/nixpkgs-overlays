1b42dbab2ae7627dded9698a80731e64d17d449b
Christian Albrecht <christian.albrecht@mayflower.de>
2020-05-25 22:57:44 +0200

    Latest pipewire with metadata.nix

--- nixpkgs/pkgs-overlays/pipewire/default.nix
+++ nixpkgs/pkgs-overlays/pipewire/default.nix
@@ -1,14 +1,17 @@
 self: pkgs:
 
+let
+  metadata = import ./metadata.nix;
+in
 pkgs.pipewire.overrideAttrs (old: rec {
-  version = "0.3.2";
+  version = metadata.rev;
 
   src = pkgs.fetchFromGitLab {
     domain = "gitlab.freedesktop.org";
     owner = "pipewire";
     repo = "pipewire";
-    rev = "7f271ef982bd3c93c39d018e4c8c45052c5e70a5";
-    sha256 = "1xyd6p43cj9jl0bqzn06myn6lsm8qwrz0hfy3szmrxl1197v5fnn";
+    rev = version; #"7f271ef982bd3c93c39d018e4c8c45052c5e70a5";
+    sha256 = metadata.sha256; #"1xyd6p43cj9jl0bqzn06myn6lsm8qwrz0hfy3szmrxl1197v5fnn";
   };
 
   buildInputs = old.buildInputs ++ (with self; [
---
+++ nixpkgs/pkgs-overlays/pipewire/metadata.nix
@@ -0,0 +1,7 @@
+{
+  repo_git = "https://gitlab.freedesktop.org/pipewire/pipewire.git";
+  branch = "master";
+  rev = "871bb47eb73d3d63c73a95da860a6defd7ffcff4";
+  sha256 = "0abas746agvp3kf5n8rpjysslksz9ss9cgxkbr67d4yz6risbpr4";
+  revdate = "2020-05-25 20:44:47Z";
+}
