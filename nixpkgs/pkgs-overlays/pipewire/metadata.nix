39c17e85351da9271bdedc1daea95010252017fb
Christian Albrecht <christian.albrecht@mayflower.de>
2020-07-04 21:01:44 +0200

    Update pipewire

--- nixpkgs/pkgs-overlays/pipewire/metadata.nix
+++ nixpkgs/pkgs-overlays/pipewire/metadata.nix
@@ -1,7 +1,7 @@
 {
   repo_git = "https://gitlab.freedesktop.org/pipewire/pipewire.git";
   branch = "master";
-  rev = "871bb47eb73d3d63c73a95da860a6defd7ffcff4";
-  sha256 = "0abas746agvp3kf5n8rpjysslksz9ss9cgxkbr67d4yz6risbpr4";
-  revdate = "2020-05-25 20:44:47Z";
+  rev = "21fc163c49309a04689fc4d636df4622198f5570"; #"871bb47eb73d3d63c73a95da860a6defd7ffcff4";
+  sha256 = "0w6hm8fc078dbzxbc23kvngd61slm8q9lz1baz66diamy30xglzp"; #"0abas746agvp3kf5n8rpjysslksz9ss9cgxkbr67d4yz6risbpr4";
+  revdate = "2020-07-02 17:31:47Z"; #"2020-05-25 20:44:47Z";
 }
